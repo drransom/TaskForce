94579ab8c76c34baa2a74cd5d3aa062bde8a5404
Elliot Reed <elliotreed2@gmail.com>
2015-04-07 10:53:18 -0700

    add static pages controller and move root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  def root
+
+  private
+
+  def require_signed_in!
+    true
   end
 end
