c839dc0ffac849786230b9817d4bf93e03559262
Elliot Reed <elliotreed2@gmail.com>
2015-04-07 11:27:37 -0700

    authenticate users on back end

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,25 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def current_user
+    return nil unless session[:session_token]
+    @current_user ||= User.find_by_session_token(session[:session_token])
+  end
+
+  def signed_in?
+    current_user
+  end
+
+  def sign_in!(user)
+    @current_user = user
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def sign_out!
+    current_user.try(:reset_session_token!)
+    session[:session_token] = nil
+  end
+
   def require_signed_in!
     true
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root to: 'static_pages#root'
 
+  resource :session, only: [:new, :create, :destroy]
+
 
 
 end
