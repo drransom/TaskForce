bffad191fef85038bc3d99db7238752d0a5e8f3a
Elliot Reed <elliotreed2@gmail.com>
2015-04-07 15:27:21 -0700

    seed data, guest login

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+u1 = User.create(email: "luke@starwars.com", password: "password")
+u2 = User.create(email: "leia@starwars.com", password: "password")
+u3 = User.create(email: "darthvader@starwars.com", password: "password")
+
+t1 = Task.create(owner_id: 1, title: "Destroy Death Star",
+              description: "Create a chain reaction", location: "Death Star",
+              vehicle: "x-wing", completed: "neither")
