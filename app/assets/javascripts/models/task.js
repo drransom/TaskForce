086d8e15f74e9a6f7033eeeaf6bae7a50e421464
Elliot Reed <elliotreed2@gmail.com>
2015-04-10 18:05:21 -0700

    justify text

--- app/assets/javascripts/models/task.js
+++ app/assets/javascripts/models/task.js
@@ -1,6 +1,14 @@
 "use strict";
 
 TaskForce.Models.Task = Backbone.Model.extend({
-  urlRoot: '/api/tasks'
+  urlRoot: '/api/tasks',
+
+  userMarkedComplete: function () {
+    return (this.complete === "user" || this.complete === "both")
+  },
+
+  taskerMarkedComplete: function () {
+    return (this.complete === "tasker" || this.complete === "both")
+  }
 
 });
