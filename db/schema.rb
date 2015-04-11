5d70824c17df5400f797fa065e0946033aa2560a
Elliot Reed <elliotreed2@gmail.com>
2015-04-11 15:40:41 -0700

    add rating to tasks table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150411011949) do
+ActiveRecord::Schema.define(version: 20150411223929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20150411011949) do
     t.string   "category"
     t.boolean  "user_completed",   default: false
     t.boolean  "tasker_completed", default: false
+    t.boolean  "rated",            default: false
   end
 
   add_index "tasks", ["location"], name: "index_tasks_on_location", using: :btree
