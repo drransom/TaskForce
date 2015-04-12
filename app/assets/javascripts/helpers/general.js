0b9f22168379b37d1d5bd3f0a6e57799ddfe39ca
Elliot Reed <elliotreed2@gmail.com>
2015-04-12 11:57:32 -0700

    revise navbar to be black

--- app/assets/javascripts/helpers/general.js
+++ app/assets/javascripts/helpers/general.js
@@ -1,4 +1,21 @@
 TaskForce.Helpers = {
 
-  guarantee: "100% satisfaction is guaranteed for all Taskers."
+  guarantee: "100% satisfaction is guaranteed for all Taskers.",
+
+  createTaskersFromTasks: function(tasks) {
+    var taskers = new TaskForce.Collections.Users();
+    tasks.each(function(task) {
+      var taskerId = task.get('tasker_id');
+      if (taskerId && !(taskers.find(taskerId))) {
+        var tasker = new TaskForce.Models.User({
+          id: task.get('tasker_id'),
+          name: task.get('tasker_name'),
+          profile_url: task.get('tasker_profile_url'),
+          alive: task.get('tasker_alive')
+        })
+        taskers.add(tasker)
+      }
+    })
+    return taskers
+  }
 }
