018efa963e73589184422c7e7fc80f1875917bb0
Elliot Reed <elliotreed2@gmail.com>
2015-04-13 11:03:02 -0700

    begin rewriting initialize

--- app/assets/javascripts/views/tasks/index_task.js
+++ app/assets/javascripts/views/tasks/index_task.js
@@ -5,32 +5,22 @@ TaskForce.Views.TaskIndex = Backbone.CompositeView.extend({
   initialize: function (options) {
     this.$el.append('<section class="task-area"></section>');
     this.$el.append('<section class="task-detail"></section>');
-    this.$el.append('<section class="rating-modal"></section>')
 
-    this.collection = new TaskForce.Collections.Tasks();
+    this.tasks = new TaskForce.Collections.Tasks();
     this.taskers = new TaskForce.Collections.Users();
 
-    this.collection.fetch({
+    this.tasks.fetch({
       success: function () {
-        TaskForce.Helpers.createTaskersFromTasks(this.collection, this.taskers)
+        TaskForce.Helpers.createTaskersFromTasks(this.tasks, this.taskers)
+        this.createMiniTaskSubviews();
         this.listenTo(this.taskers, 'change', this.render)
       }.bind(this)
     });
 
-    this.model = new TaskForce.Models.Task();
-    this.tasker = new TaskForce.Models.User();
-
-    var displayOptions = {
-      collection: this.collection,
-      indexModel: this.model,
-      heading: '<h2 class="tasks-header">Your tasks</h2>We\'ve created some sample tasks for you, click "New Task" to create a new one!<br>',
-      template: JST['tasks/mini_task'],
-    }
-
     this.taskDisplay = new TaskForce.Views.TaskDisplay( displayOptions );
     //this.taskDetails = new TaskForce.Views.TaskDetail( { task: this.task, user: this.user})
     this.taskRating = new TaskForce.Views.TaskRating( {
-      collection: this.collection,
+      tasks: this.tasks,
       model: this.model,
       taskers: this.taskers }
     )
@@ -41,6 +31,7 @@ TaskForce.Views.TaskIndex = Backbone.CompositeView.extend({
   },
 
   render: function () {
+
     this.attachSubviews();
     return this;
   },
@@ -51,22 +42,13 @@ TaskForce.Views.TaskDisplay = TaskForce.Views.IndexView.extend({
   //no body needed here yet
 
   events: {
-    'click button.rate-tasker' : 'addModal',
+    'click button.rate-tasker' : 'updateModel',
     'click button.mark-complete' : 'markComplete'
   },
 
   markComplete: function (event) {
     var id = $(event.currentTarget).data('id');
-    var model = this.collection.get(id)
+    var model = this.tasks.get(id)
     model.save( {user_completed: true}, {wait: true} );
   },
-
-  addModal: function(event) {
-    var modal = new TaskForce.Views.Modal({template: JST['modal_test']});
-    $('body').prepend('<section class="taskforce-modal"></section>');
-    var $modalEl = $('.taskforce-modal')
-    modal.setElement($modalEl);
-    modal.render();
-  }
-
 });
