d7531b3932859ebb60cec60197ec6a767a4d630f
Elliot Reed <elliotreed2@gmail.com>
2015-04-13 10:54:24 -0700

    create MiniTask view

---
+++ app/assets/javascripts/views/tasks/mini_task.js
@@ -0,0 +1,25 @@
+"use strict"
+
+TaskForce.Views.MiniTask = Backbone.View.extend({
+
+  initialize: function (options) {
+    this.task = options.task;
+    this.tasker = options.tasker;
+  },
+
+  template: JST['tasks/mini-task'],
+  rateTemplate: JST['modal_test'],
+
+  events: {
+    'click button .mark-complete' : 'markComplete',
+    'click button .rate-tasker' : 'rateTasker',
+  },
+
+  render: function () {
+    var content = this.template({ task: this.task, tasker: this.tasker });
+    this.$el.html(content);
+    return this;
+  }
+
+
+});
