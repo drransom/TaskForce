0afaf219cfedee9a73a459e5118311347e34d677
Elliot Reed <elliotreed2@gmail.com>
2015-04-13 20:10:42 -0700

    complete merging changes

--- app/assets/javascripts/views/user/tasker_profile.js
+++ app/assets/javascripts/views/user/tasker_profile.js
@@ -11,12 +11,8 @@ TaskForce.Views.TaskerProfile = TaskForce.Views.Modal.extend({
     this.$el.append('<section class="tasker-detail"></section>');
     this.$el.append('<section class="tasker-comments"></section>');
 
-<<<<<<< HEAD
-    this.taskerDetail = new TaskForce.Views.TaskerDetail( {model: this.model, pick_me: true });
-=======
     this.taskerDetail = new TaskForce.Views.TaskerDetail( {model: this.model,
                                                            task: this.task});
->>>>>>> refactorModal
     // this.taskerComments = new TaskForce.Views.TaskerComments( {model: this.model } );
 
     this.addSubview('.tasker-detail', this.taskerDetail);
@@ -38,20 +34,10 @@ TaskForce.Views.TaskerDetail = Backbone.CompositeView.extend({
   template: JST['tasker_detail'],
 
   initialize: function (options) {
-    debugger
     this.task = options.task;
-<<<<<<< HEAD
-    this.submit = options.submit;
-    if (options.el) {
-      this.$el = options.el
-    }
-    this.listenTo(this.model, 'change set', this.render)
-=======
->>>>>>> refactorModal
   },
 
   render: function () {
-    debugger
     var content = this.template( {user: this.model});
     this.$el.html(content);
     return this;
@@ -62,13 +48,8 @@ TaskForce.Views.TaskerDetail = Backbone.CompositeView.extend({
     this.task.set( { tasker_id: this.model.get('id') });
     this.task.save ({}, {
       success: function () {
-<<<<<<< HEAD
-        $('#tasker-detail').modal('hide');
-        Backbone.history.navigate('', {trigger: true});
-=======
         this.remove();;
         Backbone.history.navigate('', {trigger: true})
->>>>>>> refactorModal
       },
       error: function () {
         alert("something went wrong")
