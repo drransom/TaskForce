d92eba69633cdecbf1d1381f743ba12183ecc287
Elliot Reed <elliotreed2@gmail.com>
2015-04-15 17:10:22 -0700

    select planet with images

--- app/assets/javascripts/helpers/general.js
+++ app/assets/javascripts/helpers/general.js
@@ -31,10 +31,13 @@ TaskForce.Helpers = {
     return rating
   },
 
-  planetProfilePic: {
+  profilePic: {
     "Endor": "http://img4.wikia.nocookie.net/__cb20130731183030/starwars/images/1/1f/Endor.jpg",
     "Hoth": "http://img2.wikia.nocookie.net/__cb20071019040631/starwars/images/d/d1/Hoth.jpg",
     "Tatooine": "http://img2.wikia.nocookie.net/__cb20131019121937/starwars/images/b/b0/Tatooine_TPM.png",
-    "Coruscant": "http://img4.wikia.nocookie.net/__cb20100115192302/starwars/images/1/17/Coruscant-AoTCW.jpg"
+    "Coruscant": "http://img4.wikia.nocookie.net/__cb20100115192302/starwars/images/1/17/Coruscant-AoTCW.jpg",
+    "Battle Station": 'http://img2.wikia.nocookie.net/__cb20130221005853/starwars/images/thumb/9/9d/DSI_hdapproach.png/400px-DSI_hdapproach.png',
+    "Starship": 'http://img2.wikia.nocookie.net/__cb20070507224007/starwars/images/thumb/d/dc/Melfal.jpg/400px-Melfal.jpg',
+    "no vehicle": 'https://starwarsanon.files.wordpress.com/2014/02/going-to-jabbas-palace.gif'
   }
 }
