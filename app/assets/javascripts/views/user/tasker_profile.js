889cb9ed7b9d8005686ef13c683bed70923cb6de
Elliot Reed <elliotreed2@gmail.com>
2015-04-16 16:34:55 -0700

    able to display comments from single comment view

--- app/assets/javascripts/views/user/tasker_profile.js
+++ app/assets/javascripts/views/user/tasker_profile.js
@@ -9,18 +9,17 @@ TaskForce.Views.TaskerProfile = TaskForce.Views.Modal.extend({
     }
     this.task = options.task;
     this.submitButton = options.submitButton || false;
-    // this.taskerComments = new TaskForce.Views.TaskerComments( {
-    //   collection: this.comments,
-    //   templateOptions: {comments: this.comments}
-    //   } );
-    //
-    // this.model.fetch({success: function (response) {
-    //   TaskForce.Helpers.constructCommentsFromUser(this.model, this.comments)
-    //   this.comments.trigger('sync')
-    // }.bind(this)})
+    this.comments = new TaskForce.Collections.Comments();
+    this.listenTo(this.comments, 'sync', this.render)
+    this.model.fetch({
+      success: function (response) {
+        TaskForce.Helpers.constructCommentsFromUser(this.model, this.comments)
+        this.comments.trigger('sync')
+        }.bind(this)
+    })
+
     this.template = JST['tasker_detail'];
   },
-
 
   events: {
     'click' : 'closeBackdrop',
@@ -32,6 +31,7 @@ TaskForce.Views.TaskerProfile = TaskForce.Views.Modal.extend({
     this.task.set( { tasker_id: this.model.get('id') });
     this.task.save ({}, {
       success: function () {
+        this.remove();
         Backbone.history.navigate('', {trigger: true})
       }.bind(this),
       error: function () {
@@ -41,28 +41,12 @@ TaskForce.Views.TaskerProfile = TaskForce.Views.Modal.extend({
   },
 
   render: function () {
-    var content = this.template( { user: this.model, submitButton: this.submitButton });
+    var content = this.template( {
+                                  user: this.model,
+                                  submitButton: this.submitButton,
+                                  comments: this.comments
+                                  });
     this.$el.html(content);
     return this;
   }
 });
-//
-// TaskForce.Views.TaskerDetail = Backbone.CompositeView.extend({
-//
-//   events: {
-//     'click button.select-me' : 'submit'
-//   },
-//
-//
-//   initialize: function (options) {
-//     this.task = options.task;
-//     this.submitButton = options.submitButton;
-//   },
-//
-//   render: function () {
-//     var content = this.template( { user: this.model, submitButton: this.submitButton });
-//     this.$el.html(content);
-//     return this;
-//   },
-//
-// });
